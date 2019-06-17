c9bdf2df1952f971ba3645ee2b14018cca935695
AlKatori <p85dyer@gmail.com>
2019-06-16 20:17:22 -0400

    Now the program kicks off main.c, but I'm not 100% sure how to fully replicate what it was doing with registers in main.c without using the asm tags, maybe you can't?

---
+++ main.c
@@ -0,0 +1,35 @@
+
+
+const static int test = 0xDEADBEEF;
+static char hello[] = "Hello World\n";
+static char * hello_ptr = 0x2003000;
+
+register int *counter asm ("r10");
+register int *deadbeef asm ("r11");
+
+int main()
+{
+  *counter = 0;
+  *deadbeef = test;
+
+  char * ptr = hello;
+
+  while(*ptr != 0)
+  {
+	*hello_ptr = *ptr;
+	++hello_ptr;
+	++ptr;
+  }
+
+  //Stick a null terminator on.
+  *hello_ptr = 0;
+  
+  while(1)
+  {
+	++*counter;
+	//asm("ADDS r0, r0, #1");
+	//asm("LDR r1, #" (test));
+	//asm("LDR r6, #" (test));
+  }
+  return 0;
+}
