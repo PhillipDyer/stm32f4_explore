9511c8cde093cb4468aea58db707a6c674ff0216
AlKatori <p85dyer@gmail.com>
2019-06-29 18:27:06 -0400

    Building and linking with subdirectory

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 /** \brief Adding newlib stuff */
 #include <unistd.h>
 #include <string.h>
-#include "arm_m4_defs.h"
+#include "cortex_core/include/f4_disc_leds.h"
 
-void turn_on(int * gp);
-void turn_off(int * gp);
+#define DELAY_INTERVAL 0x186004
+
 void delay(const unsigned int i);
 
 int main()
 {
   
   int counter = 0;
-  int * deadbeef = (int *)0x2003000;
   int counter_address = (int)&counter;
-  
-  int * rcc_register = (int *)RCC_AHB1ENR;
-  int * gpio_moder = (int *)GPIOD_MODER;
-  int * gpio_otype = (int *)GPIOD_OTYPER;
-  int * gpio_speed = (int *)GPIOD_OSPEEDR;
-  int * gpio_pupdr = (int *)GPIOD_PUPDR;
-  int * gpio_odr = (int *)GPIOD_ODR;
 
+  f4_disc_leds_initialize();
+  
   asm("LDR r2, %0"
 	  :
 	  : "m" (counter_address)
@@ -37,35 +31,16 @@ int main()
 	  : "m" (compare_addr)
 	  );
 
-  /** Let's see if we can set up the clock enable register */
-  *rcc_register |= 0x8;  //enable gpio-d clock.
-  *gpio_moder = (*gpio_moder | 0x55000000 ) & 0x55FFFFFF;
-  *gpio_otype &= 0xFFFF0FFF;
-  *gpio_speed &= 0x00FFFFFF;
-  *gpio_pupdr &= 0x00FFFFFF;
-
-  /** string test */
-  
   while(1)
   {
 	++counter;
-	turn_on(gpio_odr);
+	f4_disc_leds_turn_on(Red);
 	delay(DELAY_INTERVAL);
-	turn_off(gpio_odr);
+	f4_disc_leds_turn_off(Red);
 	delay(DELAY_INTERVAL);
 
   }
   return 0;
-}
-
-void turn_on(int * gp)
-{
-  *gp |= 0xF000;
-}
-
-void turn_off(int * gp)
-{
-  *gp &= 0xFFFF0FFF;
 }
 
 void delay(const unsigned int i)
